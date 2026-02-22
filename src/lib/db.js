1a9ec19445150c920f67e77c853f63acae00b823
Ank09yadav <ankury114112118@gmail.com>
2026-02-22 23:21:23 +0530

    add Prisma client utility for global database access and logging.

---
+++ src/lib/db.js
@@ -0,0 +1,9 @@
+import { PrismaClient } from "@prisma/client";
+
+const globalForPrisma = global;
+
+export const db = globalForPrisma.prisma || new PrismaClient({
+    log: ['query', 'info', 'warn', 'error'],
+});
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
